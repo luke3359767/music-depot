427f623f5ad5ac5a09267d6075f2971eca74644d
luke33559767 <3359767@gmail.com>
2021-06-02 10:59:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 const port=3000;
 app.get('/',(req,res)=>{
 
-	res.send('hello');
+	res.send('hello bitch');
 });
 
 app.listen(port,()=>console.log(`example listening from port ${port}`));
