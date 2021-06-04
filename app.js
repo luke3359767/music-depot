b6c5c0dc2c940acfb4781db3d9e89c70b8a81f46
luke33559767 <3359767@gmail.com>
2021-06-03 23:06:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ app.get('/',(req,res)=>{
 
 	res.send('hello bitch');
 });
+app.get('/demo',(req,res)=>{
+	res.set('X-full-stack','4life');
+	res.status(418);
+	res.send('I prefer coffee');
+});
 
 app.listen(port,()=>console.log(`example listening from port ${port}`));
