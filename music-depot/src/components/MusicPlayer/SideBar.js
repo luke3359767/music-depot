34c09a1461dc9fb4b0cd4235896c79e9f3810be8
luke3359767 <3359767@gmail.com>
2021-10-10 03:25:49 +0000

    commit

--- music-depot/src/components/MusicPlayer/SideBar.js
+++ music-depot/src/components/MusicPlayer/SideBar.js
@@ -2,16 +2,18 @@
 /** @jsx jsx */
 import React from 'react';
 import {css,jsx} from "@emotion/react"
+import logo from '../../image/logo.png';
 
 const CSS=css`
 width: 200px;
 min-height: 100vh;
-padding: 20px;
+padding-top: 10px;
 background: #121233;
 `
 const SideBar=({children})=>{
     return(
         <div className="SideBar"  css={CSS}>
+            <img src={logo} height="50" margin-left="auto" margin-right="auto"/>
             {children}
         </div>
     );
