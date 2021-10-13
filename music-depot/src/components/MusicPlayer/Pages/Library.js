3ceeea705d1f82d07137f568d4923b403e082ae3
luke3359767 <3359767@gmail.com>
2021-10-13 04:10:15 +0000

    commit

--- music-depot/src/components/MusicPlayer/Pages/Library.js
+++ music-depot/src/components/MusicPlayer/Pages/Library.js
@@ -12,39 +12,8 @@ const CSS=css`
 const Library=()=>{
     const {state,dispatch}=useContext(StoreContext);
     
-    const [myState,setState]=useState({
-            title: '',
-            isbn:'',
-            author:'',
-            description:'',
-            published_date:'',
-            publisher:''
-          
-    });
-    const data = {
-        title: myState.title,
-        isbn: myState.isbn,
-        author: myState.author,
-        description: myState.description,
-        published_date: myState.published_date,
-        publisher: myState.publisher
-      };
-
-    axios.post('http://localhost:5000/api/books', data)
-        .then(res => {
-            setState({
-            title: '',
-            isbn:'',
-            author:'',
-            description:'',
-            published_date:'',
-            publisher:''
-        })})
-        .catch(err => {
-            console.log(`Error in CreateBook!${err}`);
-          })
     return(
-        <h1>{myState}</h1>
+        <h1>Li {state.currentPlaylist}</h1>
 
     );
 }
