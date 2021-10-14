bd66d65afbca8c27bb3b1ca3813b36b7155fc15f
luke33559767 <3359767@gmail.com>
2021-10-14 10:29:38 -0400

    cmmit

--- backend/routes/testapi.js
+++ backend/routes/testapi.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-  res.send("API IS WORKING");
+  res.json({
+    status:'api works',
+    message:'welcome to use api'
+  });
 })
 
 module.exports = router;
