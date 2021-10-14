c63166d5496a3a2456f4250d67825de1c1ad5cdd
luke33559767 <3359767@gmail.com>
2021-10-14 10:40:46 -0400

    install morgan

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express'); //Line 1
 const connectDB = require('./config/db');
 const cors= require('cors');
-
+const morgan=require('morgan');
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
 
@@ -9,6 +9,7 @@ const testapiRouter=require("./routes/testapi")
 
 // connectDB();
 app.use(cors());
+app.use(morgan("dev"));
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
 
 app.get('/', (req, res) => res.send('Hello world!'));
