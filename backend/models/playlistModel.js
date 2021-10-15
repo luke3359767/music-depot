d6522c0ed9c4c898830e0b363ac371e4774cd153
luke3359767 <3359767@gmail.com>
2021-10-15 18:29:13 +0000

    commit

--- backend/models/playlistModel.js
+++ backend/models/playlistModel.js
@@ -11,15 +11,34 @@ const playlistModel = new mongoose.Schema({
 });
 
 
-// SongList:{
+// PlaylilstList:{
 //   name:"favorite",
 //   album:"favorite.jpg",
-//   songs:[{
-//     id:"a id",
-//     name:"Attention",
-//     artist:"Artist",
-//     album:"Attention.jpg",
-//     path:"../../audio....",
-//     length:"3:31",
-//   },{},{}],
+//   songs:[1111,2222,3333,songID],
 // }
+songsList:{
+  {
+      id:"songid",
+      name:"Attention",
+      artist:"Artist",
+      album:"Attention.jpg",
+      path:"../../audio....",
+      length:"3:31",
+  },
+
+  {
+    id:"songid",
+    name:"Attention",
+    artist:"Artist",
+    album:"Attention.jpg",
+    path:"../../audio....",
+    length:"3:31",
+  },
+}
+AllSongList: {
+  {userID:"userID",
+    favorite:songsList,
+    recently:songsList,
+    mysonlist:songList,
+  },
+}
