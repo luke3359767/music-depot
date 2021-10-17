f7a03b6ada8d8efd007f8d3b0dc35a8957ce8f71
luke33559767 <3359767@gmail.com>
2021-10-17 11:15:32 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors= require('cors');
 const morgan=require('morgan');
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
-require('./models/User');
+// require('./models/User');
 
 const testapiRouter=require("./routes/testapi")
 const testdbapiRouter=require('./routes/testdbapi')
@@ -13,6 +13,35 @@ connectDB();
 app.use(cors());
 app.use(express.json())
 app.use(morgan("dev"));
+
+
+
+const users = [
+  {
+    id: "1",
+    username: "John123",
+    password: "jifjsldfjks@!#@12",
+    isAdmin: true,
+  },
+  {
+    id: "2",
+    username: "Jane",
+    password: "jjj@!#@12",
+    isAdmin: false,
+  },
+];
+
+app.post("api/login",(req, res)=>{
+    const {username,password}=req.body;
+    const user=users.find(u=>{
+        return u.username===username &&u.password===password
+    })
+    if(user){
+    res.json(user);
+    }else{
+        res.status(404).json("Username or password incorrect")
+    }
+})
 
 app.listen(port, () =>
     console.log(`Listening on port ${port}`)); 
