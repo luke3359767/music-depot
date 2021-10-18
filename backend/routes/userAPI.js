3933bcc9e5d916c92f4859193cecaab3bc69064a
luke33559767 <3359767@gmail.com>
2021-10-18 13:30:26 -0400

    commit

--- backend/routes/userAPI.js
+++ backend/routes/userAPI.js
@@ -5,14 +5,12 @@ const User = require("../models/User.js");
 router.post('/register',(req, res)=>{
     const registerInfo=req.body.registerInfo
     // const newUser= new User(registerInfo)
-    const newUser= new User({
-        username:"luke3359767",
-        email:"3359767@gmail.com",
-        bio:null,
-        image:"null",
-        hash:"",
-        salt:""
-    });
+    const sampleInfo = {
+      username: "luke3359767",
+      email: "3359767@gmail.com",
+      password:"luke0123",
+    };
+    const newUser= new User();
     try{
         newUser.save().then(doc=>{cnosole.log(doc)})
     }catch{
