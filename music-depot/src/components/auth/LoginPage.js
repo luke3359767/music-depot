a40f894de8e5a484f44f23e0c72de1be3ffd6e85
luke33559767 <3359767@gmail.com>
2021-10-23 16:17:59 -0400

    commit

--- music-depot/src/components/auth/LoginPage.js
+++ music-depot/src/components/auth/LoginPage.js
@@ -1,276 +1,91 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { Global, css, jsx } from "@emotion/react";
+import React, { useState } from 'react'
+import { Global, css, jsx } from '@emotion/react'
+import { useHistory, Link } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
-import jwt_decode from "jwt-decode";
-import Cookies from "universal-cookie";
 
-const CSS = css`
-  .container {
-    font-family: "Quicksand", sans-serif;
-  }
-
-  .login,
-  .home {
-    width: 200px;
-    height: 200px;
-    padding: 20px;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-    border: 1px solid lightgray;
-    border-radius: 10px;
-  }
-
-  form {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-  }
-
-  .formTitle {
-    color: teal;
-    font-weight: bold;
-  }
-
-  input {
-    height: 30px;
-    border: none;
-    border-bottom: 1px solid lightgray;
-  }
-
-  input:focus {
-    outline: none;
-  }
-
-  .submitButton {
-    width: 100px;
-    padding: 10px;
-    border: none;
-    border-radius: 10px;
-    background-color: teal;
-    color: white;
-    cursor: pointer;
-  }
-
-  .home {
-    width: 300px;
-    height: 250px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    text-align: center;
-  }
-
-  .deleteButton {
-    padding: 10px;
-    border: none;
-    border-radius: 10px;
-    background-color: #fff0f1;
-    color: #d95087;
-    cursor: pointer;
-  }
-
-  .deleteButton:hover {
-    background-color: #fad4d7;
-  }
-
-  .error {
-    font-size: 12px;
-    color: crimson;
-  }
-
-  .success {
-    font-size: 12px;
-    color: green;
-  }
-`;
+import './LoginPage.css'
+import logo from "../../image/logo.png"
+import { BsFillPersonFill } from 'react-icons/bs';
+import { IoMdMail } from 'react-icons/io';
+import { RiLockPasswordFill } from 'react-icons/ri';
 
 const Login = () => {
-  const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const cookies = new Cookies();
+  const [email, setEmail] = useState("");
 
-  const refreshToken = async () => {
-    try {
-      const res = await axios.post("http://localhost:5000/api/refresh", {
-        token: user.refreshToken,
-      });
-      setUser({
-        ...user,
-        accessToken: res.data.accessToken,
-        refreshToken: res.data.refreshToken,
-      });
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
-  const axiosJWT = axios.create();
+  const [loginErr, setloginErr] = useState(false);
+  const [usedEmailErr, setUsedEmailErr] = useState(false);
+  const [createSuccess, setcreateSuccess] = useState(false);
 
-  axiosJWT.interceptors.request.use(
-    async (config) => {
-      let currentDate = new Date();
-      const decodedToken = jwt_decode(user.accessToken);
-      if (decodedToken.exp * 1000 < currentDate.getTime()) {
-        const data = await refreshToken();
-        config.headers["authorization"] = "Bearer " + data.accessToken;
-        console.log('auto refresh tocken')
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+
+  let history = useHistory();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const res = await axios.post("http://localhost:5000/api/login", {
-        username,
-        password,
-      });
-      setUser(res.data);
-    } catch (err) {
-      console.log(err);
-    }
+
   };
 
-  const handleDelete = async (id) => {
-    setSuccess(false);
-    setError(false);
-    try {
-      await axiosJWT.delete("http://localhost:5000/api/users/" + id, {
-        headers: { authorization: "Bearer " + user.accessToken },
-      });
-      setSuccess(true);
-    } catch (err) {
-      setError(true);
-    }
-  };
+  const loginErrRender = (d) => {
+    return d ? (<p>Username or Password in incorrect</p>) : null
+  }
 
   return (
-    <div className="container" css={CSS}>
-      {user ? (
-        <div className="home">
-          <span>
-            Welcome to the <b>{user.isAdmin ? "admin" : "user"}</b> dashboard{" "}
-            <b>{user.username}</b>.
-          </span>
-          <span>Delete Users:</span>
-          <button className="deleteButton" onClick={() => handleDelete(1)}>
-            Delete John
-          </button>
-          <button className="deleteButton" onClick={() => handleDelete(2)}>
-            Delete Jane
-          </button>
-          {error && (
-            <span className="error">
-              You are not allowed to delete this user!
-            </span>
-          )}
-          {success && (
-            <span className="success">
-              User has been deleted successfully...
-            </span>
-          )}
+    <div className="wole-container">
+      <img src={logo} alt="no" />
+      <div className="form_wrapper">
+        <div className="form_container">
+          <div className="title_container">
+            <h2>Login</h2>
+          </div>
+          <div className="row clearfix">
+            <div className="">
+              <form onSubmit={handleSubmit}>
+                <div className="input_field">
+                  {" "}
+                  <span>
+                    <BsFillPersonFill className="icon" />
+                  </span>
+                  <input
+                    type="username"
+                    name="username"
+                    placeholder="Username"
+                    required
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                {loginErrRender(loginErr)}
+                <div className="input_field">
+                  {" "}
+                  <span>
+                    <RiLockPasswordFill className="icon" />
+                  </span>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                {loginErrRender(loginErr)}
+
+                <div className="input_field checkbox_option">
+                  <input type="checkbox" id="cb1" />
+                  <label for="cb1">Remember me</label>
+                </div>
+                <input className="button" type="submit" value="Register" />
+              </form>
+              <p><Link to="/reset">forget password</Link></p>
+            </div>
+          </div>
         </div>
-      ) : (
-        <div className="login">
-          <form onSubmit={handleSubmit}>
-            <span className="formTitle">Lama Login</span>
-            <input
-              type="text"
-              placeholder="username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit" className="submitButton">
-              Login
-            </button>
-          </form>
-        </div>
-      )}
+      </div>
     </div>
-  );
+  )
 };
 
 export default Login;
-
-// /** @jsxRuntime classic */
-// /** @jsx jsx */
-// import React, { useState } from "react";
-// import { Global, css, jsx } from "@emotion/react";
-// import { useHistory, Link } from "react-router-dom";
-
-// const Login = () => {
-//   return (
-//     <div className="login" css={CSS}>
-//       <form action="null">
-//         <div class="container">
-//           <h1>Login</h1>
-//           <p>Please fill in this form to login an account.</p>
-//           <hr />
-
-//           <label for="username">
-//             <b>Username or Email</b>
-//           </label>
-//           <input
-//             type="text"
-//             placeholder="Enter Username or Email"
-//             name="username"
-//             id="username"
-//             required
-//           />
-
-//           <label for="psw">
-//             <b>Password</b>
-//           </label>
-//           <input
-//             type="password"
-//             placeholder="Enter Password"
-//             name="psw"
-//             id="psw"
-//             required
-//           />
-
-//           <hr />
-
-//           <p>
-//             By login an account you agree to our{" "}
-//             <a href="#">Terms & Privacy</a>.
-//           </p>
-//           <button type="submit" class="loginbtn">
-//             Login
-//           </button>
-//         </div>
-
-//         <div class="container signin">
-//           <p>
-//             Do not have an account? <Link to="/register">Sign Up</Link>.
-//           </p>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// CSS = css``;
-
-// export default Login;
