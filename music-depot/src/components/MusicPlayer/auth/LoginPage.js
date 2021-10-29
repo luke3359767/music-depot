28179a66156712c2461682bce48f6cd4b54623fd
luke3359767 <3359767@gmail.com>
2021-10-29 14:44:08 +0000

    commit

--- music-depot/src/components/MusicPlayer/auth/LoginPage.js
+++ music-depot/src/components/MusicPlayer/auth/LoginPage.js
@@ -2,7 +2,7 @@
 /** @jsx jsx */
 import { useState } from 'react'
 import { jsx } from '@emotion/react'
-import { useHistory, Link } from "react-router-dom";
+import { useHistory, Link,Redirect } from "react-router-dom";
 import axios from "axios";
 import { useCookies} from "react-cookie";
 import React,{useContext} from 'react';
@@ -52,60 +52,67 @@ const Login = () => {
   }
 
   return (
-    <div className="wole-container">
-      <img src={logo} alt="no" />
-      <div className="form_wrapper">
-        <div className="form_container">
-          <div className="title_container">
-            <h2>Login</h2>
-          </div>
-          <div className="row clearfix">
-            <div className="">
-              <form onSubmit={handleSubmit}>
-                <div className="input_field">
-                  {" "}
-                  <span>
-                    <BsFillPersonFill className="icon" />
-                  </span>
-                  <input
-                    type="username"
-                    name="username"
-                    placeholder="Username"
-                    required
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-                {loginErrRender(loginErr)}
-                <div className="input_field">
-                  {" "}
-                  <span>
-                    <RiLockPasswordFill className="icon" />
-                  </span>
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                {loginErrRender(loginErr)}
-
-                <div className="input_field checkbox_option">
-                  <input type="checkbox" id="cb1" onChange={(e)=>{
-                    e.target.checked === true ? setExpiredDay(30) : setExpiredDay(1)
-                  }}/>
-                  <label htmlFor="cb1">Remember me</label>
-                </div>
-                <input className="button" type="submit" value="Register" />
-              </form>
-              <p>Forget password?  <Link to="/reset">Reset</Link></p>
-              <p>Do not have an account?  <Link to="/register">Register</Link></p>
+    <div>
+    {
+      !state.isLogin?(
+        <div className="wole-container">
+        <img src={logo} alt="no" />
+        <div className="form_wrapper">
+          <div className="form_container">
+            <div className="title_container">
+              <h2>Login</h2>
+            </div>
+            <div className="row clearfix">
+              <div className="">
+                <form onSubmit={handleSubmit}>
+                  <div className="input_field">
+                    {" "}
+                    <span>
+                      <BsFillPersonFill className="icon" />
+                    </span>
+                    <input
+                      type="username"
+                      name="username"
+                      placeholder="Username"
+                      required
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                  {loginErrRender(loginErr)}
+                  <div className="input_field">
+                    {" "}
+                    <span>
+                      <RiLockPasswordFill className="icon" />
+                    </span>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  {loginErrRender(loginErr)}
+    
+                  <div className="input_field checkbox_option">
+                    <input type="checkbox" id="cb1" onChange={(e)=>{
+                      e.target.checked === true ? setExpiredDay(30) : setExpiredDay(1)
+                    }}/>
+                    <label htmlFor="cb1">Remember me</label>
+                  </div>
+                  <input className="button" type="submit" value="Register" />
+                </form>
+                <p>Forget password?  <Link to="/reset">Reset</Link></p>
+                <p>Do not have an account?  <Link to="/register">Register</Link></p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      ):<Redirect to="/login" />
+    }
+  </div>
+   
   )
 };
 
