2feec14af4416abd535f502983320b42d4238614
luke33559767 <3359767@gmail.com>
2021-10-30 10:36:29 -0400

    cmmit

--- music-depot/src/components/MusicPlayer/index.js
+++ music-depot/src/components/MusicPlayer/index.js
@@ -138,7 +138,7 @@ const MusicPlayer=()=>{
     await axios.post("https://music-depot.tech/api/userapi/autologin")
     .then((res) => {
       dispatch({ type: "USER_LOGIN", user: res.data ,isLogin:true})
-      isLogin.current=false
+      isLogin.current=true
       console.log("Auto Login")    
     }).catch((err) => err);
     // axios
