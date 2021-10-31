62986bf9e35ad228ea8bf3413e5429221728eff0
luke33559767 <3359767@gmail.com>
2021-10-31 01:39:27 -0400

    b

--- music-depot/src/components/MusicPlayer/index.js
+++ music-depot/src/components/MusicPlayer/index.js
@@ -145,7 +145,7 @@ const MusicPlayer=()=>{
   useEffect(() => {
     if (isLogin) {
       axios.post("https://music-depot.tech/api/playlistapi/getplaylist",{}, {
-        headers: { authorization: `bearer ${accessToken.current}`}
+        headers: { authorization: "bearer " +accessToken.current}
       }).then((res) => { console.log(res) }).catch((err) => { console.log(err.response)})
     }
   },[isLogin])
