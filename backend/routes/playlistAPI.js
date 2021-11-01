4dd43d0a843f0d571981d807954c4fff99fd9a35
luke33559767 <3359767@gmail.com>
2021-11-01 15:30:36 -0400

    comit

--- backend/routes/playlistAPI.js
+++ backend/routes/playlistAPI.js
@@ -36,9 +36,8 @@ router.post('/addplaylist',verify,(req, res)=>{
   const newPlaylist=req.body.newPlaylist
   playlistSchema.findOne({ username: req.user.username }, async (err, playlist) => {
 
-    // playlist.mySongList[`${Object.keys(newPlaylist)[0]}`] == req.body.newPlaylist[`${Object.keys(newPlaylist)[0]}`]
-    await playlist.mySongList== req.body.newPlaylist
-
+    playlist.mySongList[`${Object.keys(newPlaylist)[0]}`] == req.body.newPlaylist[`${Object.keys(newPlaylist)[0]}`]
+    await playlist.markModified('mySongList')
     await playlist.save()
     res.status(200).json(req.body.newPlaylist)
   })
