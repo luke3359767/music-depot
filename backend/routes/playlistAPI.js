112cc9ed59a17c11e34334b05e574701ef7548c3
luke33559767 <3359767@gmail.com>
2021-11-02 00:34:56 -0400

    j

--- backend/routes/playlistAPI.js
+++ backend/routes/playlistAPI.js
@@ -40,7 +40,7 @@ router.post('/addplaylist',verify,(req, res)=>{
     // playlist.mySongList = newPlaylist
     playlist.markModified('mySongList')
     playlist.save()
-    res.status(200).json(newPlaylist)
+    res.status(200).json(`${ Object.keys(newPlaylist)[0]`)
     // hjjh
   })
 })
