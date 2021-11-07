aba947f0ec341172c77770db11632c71ece00bd3
luke33559767 <3359767@gmail.com>
2021-11-07 00:56:31 -0400

    conimt

--- music-depot/src/components/MusicPlayer/SideBar.js
+++ music-depot/src/components/MusicPlayer/SideBar.js
@@ -77,7 +77,7 @@ const SideBar=({children})=>{
             }
           }).then(async (res) => {
             await dispatch({ type: 'ADD_PLAYLIST', playlistItem: list })
-            toast.success('This playlist is already existed!', {
+            toast.success('Your playlist was created successfully!', {
               position: "top-center",
               autoClose: 2000,
               hideProgressBar: false,
