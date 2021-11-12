10a2771f12a47152d0410dbc407bb6789377783d
luke33559767 <3359767@gmail.com>
2021-11-12 00:50:59 -0500

    ocmit

--- music-depot/src/components/MusicPlayer/TopBar.js
+++ music-depot/src/components/MusicPlayer/TopBar.js
@@ -39,7 +39,7 @@ background:#191530;
     margin: auto 5px;
     width: 50%;
 }
-Avatar{
+.avatar{
         margin: auto 0px;
 
 }
@@ -54,7 +54,7 @@ const TopBar=()=>{
             <div className="searchBar"></div>
             <div className="profileBar">
                 <div className="profileButton">
-                    <Avatar alt={userNickname} src="/static/images/avatar/1.jpg" sx={{ width: 35, height: 35 }}/>
+                    <Avatar classname="avatar" alt={userNickname} src="/static/images/avatar/1.jpg" sx={{ width: 35, height: 35 }}/>
                     <span className="nickname"> {userNickname.length > 15 ? (userNickname.slice(0,14)+"...") : (userNickname)}</span>
                 </div>
             </div>
