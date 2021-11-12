ff9413e24891beaed1395dbfc31e8b0c310ba164
luke33559767 <3359767@gmail.com>
2021-11-12 00:21:04 -0500

    comit

--- music-depot/src/components/MusicPlayer/TopBar.js
+++ music-depot/src/components/MusicPlayer/TopBar.js
@@ -40,7 +40,7 @@ const TopBar=()=>{
             <div className="searchBar"></div>
             <div className="profileBar">
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 35, height: 35 }}/>
-                <span className="nickname">{userNickname.length > 12 ? (userNickname.slice(0,10)+"...") : (userNickname)}</span>
+                <span className="nickname">{userNickname.length > 15 ? (userNickname.slice(0,14)+"...") : (userNickname)}</span>
             </div>
             
 
