c49d3fcf4b81b98271c74b0e744315a6f8057b29
luke33559767 <3359767@gmail.com>
2021-11-18 10:02:27 -0500

    ocmit

--- music-depot/src/components/MusicPlayer/TopBar.js
+++ music-depot/src/components/MusicPlayer/TopBar.js
@@ -14,7 +14,6 @@ import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
 import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
 
@@ -117,12 +116,6 @@ const TopBar=()=>{
                     <Avatar /> My account
                 </MenuItem>
                 <Divider />
-                <MenuItem>
-                    <ListItemIcon>
-                        <PersonAdd fontSize="small" />
-                    </ListItemIcon>
-                    Add another account
-                </MenuItem>
                 <MenuItem>
                     <ListItemIcon>
                         <Settings fontSize="small" />
