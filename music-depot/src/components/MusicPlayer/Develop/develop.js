e829daec2008c94ef3e09c3bba240602eebca956
luke33559767 <3359767@gmail.com>
2021-12-14 15:09:34 -0500

    comit

--- music-depot/src/components/MusicPlayer/Develop/develop.js
+++ music-depot/src/components/MusicPlayer/Develop/develop.js
@@ -5,7 +5,7 @@ import { jsx } from '@emotion/react'
 import { useHistory, Link, Redirect } from "react-router-dom";
 import axios from "axios";
 import { useCookies } from "react-cookie";
-import React, { useContext } from 'react';
+import React, { useContext,useEffect } from 'react';
 import { StoreContext } from '../index'
 
 import logo from "../../../image/logo.png"
@@ -23,6 +23,16 @@ const Develop = () => {
 
 
     let history = useHistory();
+
+    useEffect(() => {
+        (async function () {
+            await axios.post("https://music-depot.ca/api/userapi/autologin")
+                .then(async (res) => {
+                    await dispatch({ type: "USER_LOGIN", user: res.data, isLogin: true })
+                    console.log("Auto Login")
+                }).catch((err) => history.push("/login"));
+        })()
+    }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -46,13 +56,12 @@ const Develop = () => {
     return (
         <div>
             {
-                state.isLogin ? (
                     state.user.username==="luke3359767"?(
                     <div className="wole-container">
                         Welcome, developer!
                     </div>
                     ): <Redirect to="/"/>
-                ) : <Redirect to="/login" />
+                
             }
         </div>
 
