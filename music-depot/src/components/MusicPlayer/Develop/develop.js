08d8b7f057ede9acbfb956f79d8ea0e237447b4f
luke33559767 <3359767@gmail.com>
2021-12-14 15:19:35 -0500

    comit

--- music-depot/src/components/MusicPlayer/Develop/develop.js
+++ music-depot/src/components/MusicPlayer/Develop/develop.js
@@ -32,11 +32,11 @@ const Develop = () => {
     return (
         <div>
             {/* { */}
-                    // state.user.username==="luke3359767"?(
+                    {/* state.user.username==="luke3359767"?( */}
                     <div className="wole-container">
                         Welcome, developer!
                     </div>
-                    // ): <Redirect to="/12345"/>
+                    {/* ): <Redirect to="/12345"/> */}
                 
             {/* } */}
         </div>
